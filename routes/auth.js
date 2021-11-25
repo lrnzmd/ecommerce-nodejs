ef4982a65550be96b21a917fc63f026dfe29f54c
DESKTOP-DC2R3RQ\lrnzm <Lorenzo Amodeo>
2021-11-25 22:50:57 +0100

    add jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 const User = require("../models/User")
 const CryptoJS = require("crypto-js")
-
+const jwt = require("jsonwebtoken")
 
 
 //REGISTER
@@ -25,6 +25,31 @@ router.post('/register', async (req, res) => {
     }
 })
 
+//LOGIN 
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username })
+        !user && res.status(401).json("Wrong User Name")
 
+
+        const  hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC)
+
+        const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+        OriginalPassword !== req.body.password &&
+                             res.status(401).json("Wrong credentials!!!")
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        }, process.env.JWS_SEC,{expiresIn:"3d"})
+
+        const { password, ...others } = user._doc;  
+
+        res.status(200).json({...others, accessToken})
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router
