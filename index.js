18bae392fc523ef3a948db85ba3900647212b539
Lorenzo <lrnzmd@gmail.com>
2021-12-02 18:38:47 +0100

    create product

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRouter = require('./routes/user')
 const authRouter = require('./routes/auth')
+const productsRouter = require('./routes/product')
 
 dotenv.config()
 
@@ -19,6 +20,8 @@ app.use(express.json())
 app.use('/api/users', userRouter)
 
 app.use('/api/auth', authRouter)
+
+app.use('/api/products', productsRouter)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Example app listening on port 5000!`)
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,17 @@
 const router = require("express").Router()
+const Product = require("../models/Product")
+const { verifyToken, verifyTokenandAuthorization, verifyTokenAndAdmin } = require('./verifyToken')
 
-
+//CREATE PRODUCT
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body);
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 
 module.exports = router
