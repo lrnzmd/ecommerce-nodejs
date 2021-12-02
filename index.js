f224480709913f617f480d4301ef3b0734711932
Lorenzo <lrnzmd@gmail.com>
2021-12-02 20:35:59 +0100

    end

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const authRouter = require('./routes/auth')
 const productsRouter = require('./routes/product')
 const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/order")
-
+const stripeRoute = require("./routes/stripe")
+const cors = require("cors")
 
 dotenv.config()
 
@@ -17,6 +18,8 @@ mongoose
     .catch((err) => {
         console.log(err)
     })
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -30,6 +33,7 @@ app.use("/api/carts", cartRoute)
 
 app.use("/api/orders", orderRoute)
 
+app.use("/api/checkout", stripeRoute)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Example app listening on port 5000!`)
